597f307a04d09eb9ca7ff33b1f15c12fe65354a2
Riley Mathews <rileymathews80@gmail.com>
2018-08-17 15:04:56 -0500

    got search terms passed into function

--- website/views/list_product_view.py
+++ website/views/list_product_view.py
@@ -5,6 +5,7 @@ from website.models import Product
 
 
 def list_products(request):
+    print(request.GET.get('search_terms', ''))
     all_products = Product.objects.all()
     template_name = 'product/list.html'
     return render(request, template_name, {'products': all_products})
