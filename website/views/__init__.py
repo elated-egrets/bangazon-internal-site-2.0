169de2c8b066a08e64e84550a048f51826e4978c
Riley Mathews <rileymathews80@gmail.com>
2018-08-17 14:47:57 -0500

    fixed merge conflict

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -2,8 +2,5 @@ from .list_product_view import list_products
 from .sell_product_view import sell_product
 from .auth_view import register, login_user, user_logout
 from .index_view import index
-<<<<<<< HEAD
 from .add_payment_type_view import add_payment_type
-=======
 from .product_detail_view import product_detail
->>>>>>> 8f53de34090e5dde3a45e5da953b35b7f826cf80
