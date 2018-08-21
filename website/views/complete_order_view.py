aacd7c373b41a7f6fa6ff8bbce56f1358c4c4451
Riley Mathews <rileymathews80@gmail.com>
2018-08-21 15:48:30 -0500

    added code for completing an order

--- website/views/complete_order_view.py
+++ website/views/complete_order_view.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from website.models import PaymentType
+import datetime
 
 """  
     module: complete order view
@@ -8,4 +10,21 @@ from django.shortcuts import render
 
 def complete_order_view(request):
     """ function to generate view for completing an order """
-    return render(request, 'website/complete_order.html', {})
+    print(request.POST.get('payment'))
+    current_user = request.user
+
+    if request.method == "GET":
+        payment_types = current_user.paymenttype_set.all()
+        return render(request, 'website/complete_order.html', {'payment_types': payment_types})
+    elif request.method == "POST":
+        # get the open order
+        open_order = next((order for order in current_user.order_set.all() if order.date_closed == None), None)
+        # get the payment based on id from the form
+        payment_selected = PaymentType.objects.get(pk=request.POST.get('payment'))
+        # set the order payment and date closed
+        open_order.payment_type = payment_selected
+        open_order.date_closed = datetime.datetime.now()
+        # save the order
+        open_order.save()
+        # redirect the user back to their cart
+        return redirect('/cart')
