c032464875020391a0e76a50f69b0ef5a9c570da
Ryoung27 <RRYoung89@gmail.com>
2018-08-21 13:02:07 -0500

    cart cancel

--- website/urls.py
+++ website/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^my_products$', views.list_my_products, name='list_my_products'),
     url(r'^products/(?P<product>[0-9]+)$', views.product_detail, name='product_detail'),
     url(r'^payment_type/delete/(?P<payment>[0-9]+)/$', views.delete_payment_type, name='delete_person'),
-    url(r'^my_products/delete/(?P<product>[0-9]+)/$', views.delete_my_product, name='delete_my_product')
+    url(r'^my_products/delete/(?P<product>[0-9]+)/$', views.delete_my_product, name='delete_my_product'),
+    url(r'^cart$', views.order_view, name='order_view')
 ]
