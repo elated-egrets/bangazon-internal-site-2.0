c62ef33df6760124254fbc3dd0004cac088d6ff8
Riley Mathews <rileymathews80@gmail.com>
2018-08-22 09:24:24 -0500

    started logic for counting how many products are left from a seller

--- website/views/list_my_products.py
+++ website/views/list_my_products.py
@@ -1,7 +1,8 @@
 from website.forms import UserForm, ProductForm
 from django.shortcuts import render
+from collections import Counter
 
-from website.models import Product
+from website.models import Product, Order
 from django.contrib.auth.decorators import login_required
 
 
@@ -17,7 +18,12 @@ def list_my_products(request):
 
     Written By: Katheryn Ford
     """
+    # get all orders
+    # create an empty dict, keys will be product ids, and value will be how many have sold
+    product_counter = Counter([product for order in Order.objects.all() for product in order.products.all()])
+    # loop through orders and loop through products, for each one add one to the value of the id on the dict
+    print(product_counter)
     current_user = request.user
     filtered_products = Product.objects.filter(seller_id=current_user)
     template_name = 'product/my_products.html'
-    return render(request, template_name, {'products': filtered_products})
+    return render(request, template_name, {'products': filtered_products, 'product_counter': product_counter})
