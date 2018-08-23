6aeccc2a2f803b3fa62abeef267afa525b9c5911
Riley Mathews <rileymathews80@gmail.com>
2018-08-23 13:12:23 -0500

    added login required views

--- website/views/profile_view.py
+++ website/views/profile_view.py
@@ -1,9 +1,9 @@
 from website.forms import UserForm
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from website.models import Profile, Order
 
-
+@login_required
 def profile_view(request):
     '''View to access the profile along with user info thats auto populated by Django
     Returns:
